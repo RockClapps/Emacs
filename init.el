104dd7275be3961ed780e784e24be62599cda525
Rock Clapps <rockclapps@gmail.com>
2024-02-02 13:17:12 -0500

    Emacs

---
+++ init.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(zenburn))
+ '(custom-safe-themes
+   '("f366d4bc6d14dcac2963d45df51956b2409a15b770ec2f6d730e73ce0ca5c8a7" default))
+ '(indent-tabs-mode nil)
+ '(initial-buffer-choice t)
+ '(make-backup-files nil)
+ '(package-selected-packages '(zenburn-theme magit evil))
+ '(standard-indent 2)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(evil-mode)
