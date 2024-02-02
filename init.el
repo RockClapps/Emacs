020910556e360587672c03b798a04beb18b8ceb5
Rock Clapps <rockclapps@gmail.com>
2024-02-02 13:26:30 -0500

    Edited README and updated some settings

--- init.el
+++ init.el
@@ -3,14 +3,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-save-default nil)
  '(custom-enabled-themes '(zenburn))
  '(custom-safe-themes
    '("f366d4bc6d14dcac2963d45df51956b2409a15b770ec2f6d730e73ce0ca5c8a7" default))
  '(indent-tabs-mode nil)
  '(initial-buffer-choice t)
- '(make-backup-files nil)
  '(package-selected-packages '(zenburn-theme magit evil))
+ '(savehist-mode t)
  '(standard-indent 2)
+ '(tab-always-indent t)
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
