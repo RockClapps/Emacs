e5741a5d58a1521b7a01766c2b1dcdfe96dd24ae
Rock Clapps <rockclapps@gmail.com>
2024-02-03 23:25:52 -0500

    Added Go

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
    '("f366d4bc6d14dcac2963d45df51956b2409a15b770ec2f6d730e73ce0ca5c8a7" default))
  '(indent-tabs-mode nil)
  '(initial-buffer-choice t)
- '(package-selected-packages '(zenburn-theme magit evil))
+ '(package-selected-packages '(go-mode zenburn-theme magit evil))
  '(savehist-mode t)
  '(standard-indent 2)
  '(tab-always-indent t)
